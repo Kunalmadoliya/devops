fdecfdc3817d99d4f0e1353ef969af421e67f75d
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-06-07 16:00:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express();
+const PORT = process.env.PORT ?? 8000;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  return res.json({message: "Server is healthy"});
+});
+
+
+app.listen(PORT , ()=> {
+    console.log(`APP IS LISTEN ON  PORT http://localhost:${PORT}`)
+})
